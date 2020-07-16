31d884a20050eb7c22642dee838043f1fd987d8d
Николай Васильев <60035927+Kolyanbodymass@users.noreply.github.com>
2020-07-16 23:17:06 +0500

    add modules: mask and checkTextInputs

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modules/modals';
 import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -8,5 +11,8 @@ window.addEventListener('DOMContentLoaded', () => {
     modals();
     sliders('.main-slider-item', 'vertical');
     sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
-
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
 });
